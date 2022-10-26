9a20cb7877a0e0a76df18d731188f9bd31d50559
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-26 09:51:06 +0300

    added auth:sanctum middleware to store, update and destroy functions

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,6 +9,12 @@ use Exception;
 
 class TaskController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum', ['only' => ['store', 'update', 'destroy']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
